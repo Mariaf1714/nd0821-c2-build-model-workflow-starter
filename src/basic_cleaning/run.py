f45af1d65958ea8e81028a71ab48fa58210072b3
Mariaf1714 <maria_frik@web.de>
2022-02-08 12:09:35 +0100

    Debugging basic_cleaning step

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,9 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.5, 41.2)
+
+    df = df[idx].copy()
     df.to_csv("clean_sample.csv", index=False)
 
     logger.info(f"Uploading {args.output_artifact} to Weights & Biases")
